6a0c431c80fd3504eaa6639ec7c2acc97057471d
GaperNew <danya.novlyanskiy.99@mail.ru>
2022-11-21 14:28:36 +0300

    added doctrings, the code overall cleaned

---
+++ practice2.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
+          'квадратного корня из заданного числа'
+
+
+def CalculateSquareRoot(Number):
+    """Вычисляет квадратный корень"""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    """Проверяет на неотрицательность
+    возвращает корень
+    """
+    root: int = 0
+    if your_number <= 0:
+        return
+    else:
+        root = CalculateSquareRoot(your_number)
+    print(f"Мы вычислили квадратный корень из введённого вами числа. "
+          f"Это будет: {root}")
+
+
+print(message)
+calc(25.5)
