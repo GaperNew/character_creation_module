0dec5a2bab991d1ade8c6063c658a7f4efd4fe50
GaperNew <danya.novlyanskiy.99@mail.ru>
2022-11-25 14:03:41 +0300

    add working version without classes

---
+++ practice2.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
+          'квадратного корня из заданного числа'
+
+
+def calculate_square_root(Number):
+    """Вычисляет квадратный корень."""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    """Проверяет на неотрицательность.
+
+    Возвращает корень.
+    """
+    root: int = 0
+    if your_number <= 0:
+        return
+    else:
+        root = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {root}')
+
+
+print(message)
+calc(25.5)
